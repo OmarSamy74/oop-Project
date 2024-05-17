2f77074bfe2fb774c0f3ca320a597645aff994aa
Momen-Sanad <s-momen.sanad@zewailcity.edu.eg>
2024-05-17 22:09:38 +0300

    Update Event.cpp

--- Project1/Event.cpp
+++ Project1/Event.cpp
@@ -1,130 +1,104 @@
 #include "Event.h"
-class Event{
 
-    private:
+void Event::rush() {
+    cout << "Successfully stated being in a rush.\nExtra fees will be applied.\n";
+    // add code to increase total order price by 5%
+}
 
-    int choice;
-    Guess_Game player;
-    TicTacToe game;
-    Menu menu;
-    string recommendation;
+void Event::clearTerminal() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
 
-    public:
+void Event::startup() {
+    cout << "Welcome to Baladina!\nOur services:\n"
+        "1. Credit card services.\n"
+        "2. Let us get to know you.\n"
+        "3. Edit personal information\n"
+        "4. Order.\n"
+        "Please enter the number of your desired service or 'q' to quit: ";
+}
 
-    void rush(){
-
-        cout << "Succesfully stated being in a rush.\n Extra fees will be applied.\n";
-        // add code to increase total order price by 5%
-
+void Event::save_data(vector<Customer> customers) {
+    ofstream outfile("customer_data.txt");
+    if (!outfile) {
+        cerr << "Error opening file for writing\n";
+        return;
     }
+    for (Customer customer : customers) {
+        outfile << "Name: " << customer.getName() << endl;
+        outfile << "Age: " << customer.getAge() << endl;
+        outfile << "Gender: " << customer.getGender() << endl;
+        outfile << "ID: " << customer.getID() << endl;
 
-    void clearTerminal() {
-
-        #ifdef _WIN32           /* # to define a preprocessor directive (processed before actual code)
-                                    if the _WIN32 macro is defined --> use cls*/
-            system("cls");
-        #else
-            system("clear");    // For other os
-        #endif
+        vector<double> loyalty_points = customer.getLoyaltyPoints();
+        if (!loyalty_points.empty()) {
+            outfile << "Loyalty Points: ";
+            outfile << endl;
         }
 
-    void startup() {
-
-        cout << "Welcome to Baladina!\nOur services:\n"
-            "1. Credit card services.\n"
-            "2. Let us get to know you.\n"
-            "3. Edit personal information\n"
-            "4. Order.\n"
-            "Please enter the number of your desired service or 'q' to quit: ";
+        outfile << endl;
     }
 
-    void save_data(vector<Customer>customers) {
-        //open file in write mode
-        ofstream outfile("customer_data.txt");
-        //test file opening
-        if (!outfile) {
-            cerr << "Error opening file for writing\n";
-            return;
-        }
-        // check each customer in vector
-        for (Customer customer : customers) {        // range based loop
-            // write each customer component
-            outfile << "Name: " << customer.getName() << endl;
-            outfile << "Age: " << customer.getAge() << endl;
-            outfile << "Gender: " << customer.getGender() << endl;
-            outfile << "ID: " << customer.getID() << endl;
+    outfile.close();
+}
 
-            vector<double> loyalty_points = customer.getLoyaltyPoints();
-            // check if loyalty points are empty
-            if (!loyalty_points.empty()) {
-                outfile << "Loyalty Points: ";
-                outfile << endl;
-                }
+void Event::waiting() {
+    do {
+        cout << "\n"
+            << "Enter your choice:\n"
+            << "\n"
+            << "1. Guess Game\n"
+            << "2. Tic-Tac-Toe\n"
+            << "3. Are you in a rush?\n"
+            << "4. Any menu items recommendations?\n"
+            << "5. Wait the remaining time.\n";
 
-            outfile << endl; // write new line after each customer
-        }
+        cin >> choice;
 
-            outfile.close();
-    }
+        cin.clear();
+        fflush(stdin);
 
-    void waiting() {
+        switch (choice) {
+            case 1:
+                player.playQuiz();
+                break;
 
+            case 2:
+                game.playTicTacToe();
+                break;
 
-            do {
+            case 3:
+                rush();
+                break;
 
-                cout << "\n"
-                    << "Enter your choice:\n"
-                    << "\n"
-                    << "1. Guess Game\n"
-                    << "2. Tic-Tac-Toe\n"
-                    << "3. Are you in a rush?\n"
-                    << "4. Any menu items recommendations?\n"
-                    << "5. Wait the remaining time.\n";
-
-                    cin >> choice;
-
-                cin.clear();
-                fflush(stdin);
-
-                switch (choice) {
-
-                case 1:
-
-                    player.playQuiz();
-
-                case 2:
-
-                    game.playTicTacToe();
-
-                case 3:
-
-                    rush();
-
-                case 4:
-
-                    cout << "Enter your menu recommendation (one item per line, type 'done' to finish):\n";
-
-                    while (true) {
-                        cin >> recommendation;
-                        //convert to uppercase
-                        for (char &c : recommendation) {
-                            c = toupper(c);
-                        }
-
-                        if (recommendation == "DONE") 
-                            break;
+            case 4:
+                cout << "Enter your menu recommendation (one item per line, type 'done' to finish):\n";
+                
+                while (true) {
+                    cin >> recommendation;
+                    //convert to uppercase
+                    for (char &c : recommendation) {
+                        c = toupper(c);
                     }
-                    cout << "Thank you for your recommendation.\n";
-                    menu.addRecommendation(recommendation);
 
-                case 5:
-                    cout << "Thanks for visiting!\n";
-                    break;
-
-                default:
-                    cout << "Invalid choice\n";
+                    if (recommendation == "DONE") 
+                        break;
                 }
+                cout << "Thank you for your recommendation.\n";
+                menu.addRecommendation(recommendation);
+                break;
 
-            } while (choice != 5);
+            case 5:
+                cout << "Thanks for visiting!\n";
+                break;
+
+            default:
+                cout << "Invalid choice\n";
         }
-    };
+
+    } while (choice != 5);
+}
